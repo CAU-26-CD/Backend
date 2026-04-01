307b750ed491fe31962a7061dd4000c6769ad82a
lyemee <lyemee23@cau.ac.kr>
2026-04-01 19:40:55 +0900

    Implement timestamp-synced feedback flow with mobile and PC pages, simple frontend page implmented

--- app/main.py
+++ app/main.py
@@ -25,15 +25,21 @@
 from fastapi import FastAPI
 
 from app.database import Base, engine
-from app.routers import feedbacks, sessions, users
+from app.routers import feedbacks, sessions, users, pages
+from fastapi.staticfiles import StaticFiles
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Rehearsal Feedback API")
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 app.include_router(users.router)
 app.include_router(sessions.router)
 app.include_router(feedbacks.router)
+
+## html페이지 띄워주는 라우터
+app.include_router(pages.router)
 
 
 @app.get("/")
