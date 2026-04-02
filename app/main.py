acd8c6a8a5e2771545fdc82c2186a6c13e29f6df
lyemee <lyemee23@cau.ac.kr>
2026-04-02 10:05:54 +0900

    recording, uploading implemented(#2)

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@
 from fastapi import FastAPI
 
 from app.database import Base, engine
-from app.routers import feedbacks, sessions, users, pages
+from app.routers import feedbacks, sessions, users, pages, videos
 from fastapi.staticfiles import StaticFiles
 
 Base.metadata.create_all(bind=engine)
@@ -40,6 +40,7 @@ app.include_router(feedbacks.router)
 
 ## html페이지 띄워주는 라우터
 app.include_router(pages.router)
+app.include_router(videos.router)
 
 
 @app.get("/")
